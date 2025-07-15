587eb590fdcdcd76b5852dd1e18414abe559f2ac
Haruka <120457320+Haruukaaa@users.noreply.github.com>
2025-07-15 07:31:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ print_modname() {
   sleep 1
   ui_print "Codename           : Kafka               "
   sleep 1
-  ui_print "Created            : Hirauki"
+  ui_print "Created            : Haruka"
   sleep 1
-  ui_print "Publisher          : Hirauki"
+  ui_print "Publisher          : Haruka"
   sleep 1
   ui_print "Update             : https://t.me/hgane_rei"
   sleep 1
