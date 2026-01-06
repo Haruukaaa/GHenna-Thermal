0f1d6b3b169bb01d064aa4aa447c89c886587c34
Hirauki <120457320+Hirauki@users.noreply.github.com>
2026-01-06 09:56:34 +0700

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,4 +1,4 @@
-##!/system/bin/sh
+#!/system/bin/sh
 
 # GHenna Liv
 wait_until_login() {
@@ -41,7 +41,7 @@ sh /system/etc/.nth_fc/.fc_main.sh
 
 ext 5500000 /sys/class/power_supply/battery/constant_charge_current_max
 
-su -lp 2000 -c "cmd notification post -S bigtext -t 'Empyrea' 'Tag' 'Morning, $(getprop ro.product.board). Long time  waiting for you to wake up.'"
+su -lp 2000 -c "cmd notification post -S bigtext -t 'Empyrea' 'Tag' '$(getprop ro.product.board) Heat cost reduced, I need to be faster..'"
 
 sleep 1
 rm -f /storage/emulated/0/*.log;
@@ -51,8 +51,8 @@ dumpsys deviceidle enable light
 dumpsys deviceidle enable deep
 settings put global device_idle_constants
 sleep 1
-su -c pm disable com.google.android.gms/.chimera.GmsIntentOperationService
-su -c pm disable com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver
+su -c "pm disable com.google.android.gms/.chimera.GmsIntentOperationService"
+su -c "pm disable com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver"
 cmd accessibility stop-trace
 cmd migard dump-trace false
 cmd migard start-trace false
@@ -72,27 +72,30 @@ change_task_cgroup "android.hardware.graphics.composer" "top-app" "cpuset"
 change_task_cgroup "android.hardware.graphics.composer" "foreground" "stune"
 change_task_nice "surfaceflinger" "-20"
 change_task_nice "android.hardware.graphics.composer" "-20"
-setprop debug.sf.hw 1
-setprop debug.sf.latch_unsignaled 1
-setprop debug.sf.prime_shader_cache.solid_layers true;
-setprop debug.sf.prime_shader_cache.shadow_layers true;
-setprop debug.sf.prime_shader_cache.image_layers true;
-setprop debug.sf.prime_shader_cache.clipped_layers true;
-setprop debug.sf.prime_shader_cache.edge_extension_shader true;
-setprop debug.sf.prime_shader_cache.hole_punch false;
-setprop debug.sf.prime_shader_cache.solid_dimmed_layers false;
-setprop debug.sf.prime_shader_cache.image_dimmed_layers false;
-setprop debug.sf.prime_shader_cache.pip_image_layers false;
-setprop debug.sf.prime_shader_cache.transparent_image_dimmed_layers false;
-setprop debug.sf.prime_shader_cache.clipped_dimmed_image_layers false
 change_task_nice "kswapd" "-2"
 change_task_nice "oom_reaper" "-2"
 change_task_affinity "kswapd" "7f"
 change_task_affinity "oom_reaper" "7f"
+
+setprop debug.sf.hw 1
+setprop debug.sf.latch_unsignaled 1
+setprop debug.sf.prime_shader_cache.solid_layers true
+setprop debug.sf.prime_shader_cache.shadow_layers true
+setprop debug.sf.prime_shader_cache.image_layers true
+setprop debug.sf.prime_shader_cache.clipped_layers true
+setprop debug.sf.prime_shader_cache.edge_extension_shader true
+setprop debug.sf.prime_shader_cache.hole_punch false
+setprop debug.sf.prime_shader_cache.solid_dimmed_layers false
+setprop debug.sf.prime_shader_cache.image_dimmed_layers false
+setprop debug.sf.prime_shader_cache.pip_image_layers false
+setprop debug.sf.prime_shader_cache.transparent_image_dimmed_layers false
+setprop debug.sf.prime_shader_cache.clipped_dimmed_image_layers false
+
 for queue in /sys/block/*/queue; do
     echo "0" > "$queue/iostats"
-    done
-    for h in /proc/sys/kernel; do
+done
+
+for h in /proc/sys/kernel; do
    echo "0" > "$h/perf_event_paranoid"
    echo "off" > "$h/printk_devkmsg"
    echo "0" > "$h/sched_latency_ns"
@@ -105,13 +108,30 @@ for queue in /sys/block/*/queue; do
    echo "0" > "$h/sched_tunable_scaling"
    echo "0" > "$h/panic"
    echo "0" > "$h/panic_on_oops"
+   echo "0" "$h/panic_on_warn
+   echo "0" "$h/panic_on_rcu_stall
    echo "2" > "$h/sched_rr_timeslice_ms"
    echo "0" > "$h/sched_energy_aware"
    echo "1" > "$h/sched_util_clamp_min"
    echo "1" > "$h/sched_util_clamp_min_rt_default"
    echo "2" > "$h/sched_pelt_multiplier"
    echo "1000000" > "$h/sched_rt_period_us"
+   echo "0" > "$h/hung_task_timeout_secs"
+   echo "0" > "$h/debug_locks"
+   echo "0" > "$h/sched_schedstats"
+   echo "0" > "$h/split_lock_mitigate"
+   echo "32" > "$h/sched_nr_migrate"
+   echo "0" > "$h/perf_event_paranoid"
+   echo "1" > "$h/sched_child_runs_first"
+   echo "4000000" > "$h/sched_latency_ns"
+   echo "0" > "$h/sched_autogroup_enabled"
+   echo "3" > "$h/perf_cpu_time_max_percent"
+   echo "50000" > "$h/sched_migration_cost_ns"
+   echo "1000000" > "$h/sched_min_granularity_ns"
+   echo "0" > "$h/sched_min_task_util_for_colocation"
+   echo "1500000" > "$h/sched_wakeup_granularity_ns"
 done
+
 echo "0" > /sys/kernel/msm_thermal/enabled
 echo "0" > /sys/class/kgsl/kgsl-3d0/throttling
 echo "N" > /sys/module/msm_thermal/parameters/enabled
@@ -125,9 +145,7 @@ chmod 444 /sys/module/qti_haptics/parameters/vmax_mv_override
 
 echo "0" > /sys/kernel/rcu_normal
 echo "0" > /sys/kernel/rcu_expedited
-echo "1" > /proc/sys/kernel/timer_migration
 echo "0" > /sys/devices/system/cpu/isolated
-echo "0" > /proc/sys/kernel/hung_task_timeout_secs
 
 echo "1" > /dev/stune/top-app/schedtune.boost
 echo "0" > /dev/stune/top-app/schedtune.prefer_idle
@@ -138,29 +156,14 @@ echo "0" > /sys/kernel/ccci/debug
 echo "0" > /sys/kernel/debug/rpm_log
 echo "0" > /proc/sys/vm/page-cluster
 echo "120" > /proc/sys/vm/stat_interval
-echo "0" > /proc/sys/kernel/debug_locks
 echo "0" > /sys/kernel/tracing/tracing_on
-echo "0" > /proc/sys/kernel/sched_schedstats
-echo "0" > /proc/sys/kernel/split_lock_mitigate
-echo "32" > /proc/sys/kernel/sched_nr_migrate
-echo "0" > /proc/sys/kernel/perf_event_paranoid
-echo "1" > /proc/sys/kernel/sched_child_runs_first
-echo "0" > /proc/sys/kernel/sched_tunable_scaling
 echo "0" > /proc/sys/vm/compaction_proactiveness
-echo "4000000" > /proc/sys/kernel/sched_latency_ns
-echo "0" > /proc/sys/kernel/sched_autogroup_enabled
-echo "3" > /proc/sys/kernel/perf_cpu_time_max_percent
-echo "50000" > /proc/sys/kernel/sched_migration_cost_ns
+
 echo "0" > /sys/module/mmc_core/parameters/use_spi_crc
-echo "1000000" > /proc/sys/kernel/sched_min_granularity_ns
 echo "0" > /sys/module/cpufreq_bouncing/parameters/enable
-echo "0" > /proc/sys/kernel/sched_min_task_util_for_colocation
-echo "1500000" > /proc/sys/kernel/sched_wakeup_granularity_ns
 echo "0" > /proc/task_info/task_sched_info/task_sched_info_enable
 echo "0" > /proc/oplus_scheduler/sched_assist/sched_assist_enabled
 
-echo "0 0 0 0" > /proc/sys/kernel/printk
-echo "off" > /proc/sys/kernel/printk_devkmsg
 echo "0" > /sys/module/printk/parameters/pid
 echo "0" > /sys/module/printk/parameters/cpu
 echo "0" > /sys/module/printk/parameters/time
@@ -169,10 +172,6 @@ echo "N" > /sys/module/sync/parameters/fsync_enabled
 echo "1" > /sys/module/printk/parameters/ignore_loglevel
 echo "0" > /sys/module/printk/parameters/printk_ratelimit
 echo "1" > /sys/module/printk/parameters/console_suspend
-echo "0" /proc/sys/kernel/panic
-echo "0" /proc/sys/kernel/panic_on_oops
-echo "0" /proc/sys/kernel/panic_on_warn
-echo "0" /proc/sys/kernel/panic_on_rcu_stall
 echo "0" /sys/module/kernel/parameters/panic
 echo "0" /sys/module/kernel/parameters/panic_on_warn
 echo "0" /sys/module/kernel/parameters/pause_on_oops
@@ -186,5 +185,5 @@ echo "0" > /sys/kernel/debug/dri/0/debug/enable
 echo "1" > /sys/module/spurious/parameters/noirqdebug
 echo "0" > /sys/kernel/debug/sde_rotator0/evtlog/enable
 
-su -lp 2000 -c "cmd notification post -S bigtext -t 'Empyrea' 'Tag' 'Thank you, $(getprop ro.product.board).'"
-    exit 0
+su -lp 2000 -c "cmd notification post -S bigtext -t 'Empyrea' 'Tag' '$(getprop ro.product.board) I would not forgive myself for not being there when they need me.'"
+exit 0
